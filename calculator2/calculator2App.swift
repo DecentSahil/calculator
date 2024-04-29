a1251b33de547484bce65fb84d2fd881cef671bd
Students <students@SAITMs-iMac-101.local>
2024-04-29 14:04:02 +0530

    calculator app

---
+++ calculator2/calculator2App.swift
@@ -0,0 +1,17 @@
+//
+//  calculator2App.swift
+//  calculator2
+//
+//  Created by Students on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct calculator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
